2649689f03cb6f4e4e66ac61ebc76d2d4825c201
Taariq Hassan <thassan743@gmail.com>
2018-06-02 14:48:50 +0200

    Test wall_follower

---
+++ ShellScripts/wall_follower.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Launch the turtlebot in a gazebo environment
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/RoboND-HomeServiceRobot-Project/home_service_robot/worlds/MyWorld.world" &
+sleep 5
+#launch gmapping_demo to perform SLAM
+xterm  -e  " roslaunch home_service_robot gmapping.launch" & 
+sleep 5
+#Launch view_navigation to view the map in rviz
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+#Launch keyboard_teleop to manually control the robot with keyboard commands
+xterm -e   " rosrun wall_follower wall_follower"
+
